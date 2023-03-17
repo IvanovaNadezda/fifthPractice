95872fdc86170ef4dcb7dd7172d1c8fea93697f6
inade <inadezda2003@gmail.com>
2023-03-17 23:44:20 +0300

    final

---
+++ app/src/main/java/com/example/fifthpractice/FragmentScreen3.java
@@ -0,0 +1,42 @@
+package com.example.fifthpractice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.fifthpractice.databinding.Screen3Binding;
+
+
+public class FragmentScreen3 extends Fragment {
+    Screen3Binding binding;
+    public FragmentScreen3()
+    {
+        super(R.layout.screen3);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = Screen3Binding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Bundle bundle = new Bundle();
+                bundle.putFloat("rating3",  binding.ratingBar.getRating());
+                Navigation.findNavController(view).navigate(R.id.action_third_fragment_to_first_fragment,bundle);
+            }
+        });
+
+    }
+}
